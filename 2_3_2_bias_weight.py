5ef93d55d5671699d4af655c811888b3fe3f43b6
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-06-23 20:52:26 +0900

    2.3.3 重みとバイアスによるANDゲート実装

--- 2_3_2_bias_weight.py
+++ 2_3_2_bias_weight.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-x = np.array([0, 1])
-w = np.array([0.5, 0.5])
-b = -0.7
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
-print(w*x)
-print(np.sum(w*x))
-print(np.sum(w*x) + b)
+print(AND(0, 1))
