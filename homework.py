3bd7db38ee3dfe252bf944382b53e395f4992d52
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-07-21 13:15:09 +0900

    3章まで実装

--- homework.py
+++ homework.py
@@ -79,7 +79,114 @@ def forward(network, x):
 
     return y
 
+# network = init_network()
+# x = np.array([0.1, 0.5])
+# y = forward(network, x)
+# print(y)
+
+# 3.5 出力層の設計
+# 3.5.1 恒等関数とソフトマックス関数
+# def softmax(a):
+#     exp_a = np.exp(a)
+#     sum_exp_a = np.sum(exp_a)
+#     y = exp_a / sum_exp_a
+#
+#     return y
+
+# 3.5.2 ソフトマックス関数の実装上の注意
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+# 3.5.3 ソフトマックス関数の特徴
+
+# 3.5.4出力層のニューロンの数
+
+# 3.6 手書き数字認識
+# 3.6.1 MNISTデータセット
+import sys, os
+import pickle
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+from PIL import Image
+
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+#
+# img = x_test[0]
+# label = t_train[0]
+#
+# print(label)
+#
+# print(img.shape)
+# img = img.reshape(28, 28)
+# print(img.shape)
+#
+# img_show(img)
+
+# 3.6.2 ニューラルネットワークの推論処理
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+# x, t = get_data()
+# network = init_network()
+#
+# accuracy_cnt = 0
+# for i in range(len(x)):
+#     y = predict(network, x[i])
+#     p = np.argmax(y)
+#
+#     if p == t[i]:
+#         accuracy_cnt += 1
+#
+# print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+# 3.6.3 バッチ処理
+x, t = get_data()
 network = init_network()
-x = np.array([0.1, 0.5])
-y = forward(network, x)
-print(y)
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
