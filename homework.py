eb1b9cbb42439ab1bc5215dee8d81099faaad96b
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-07-24 23:45:48 +0900

    4賞まで実装

--- homework.py
+++ homework.py
@@ -322,19 +322,125 @@ class simpleNet:
 
         return loss
 
-net = simpleNet()
-print(net.W)
+# net = simpleNet()
+# print(net.W)
+#
+# x = np.array([0.6, 0.9])
+# p = net.predict(x)
+# print(p)
+# print(np.argmax(p))
+# t = np.array([0, 0, 1])
+# print(net.loss(x, t))
+#
+# # def f(W):
+# #     return net.loss(x, t)
+#
+# f = lambda w: net.loss(x, t)
+# dW = numerical_gradient(f, net.W)
+# print(dW)
 
-x = np.array([0.6, 0.9])
-p = net.predict(x)
-print(p)
-print(np.argmax(p))
-t = np.array([0, 0, 1])
-print(net.loss(x, t))
+# 4.5 学習アルゴリズムの実装
+# 4.5.1 2層ニューラルネットワークのクラス
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
-# def f(W):
-#     return net.loss(x, t)
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
 
-f = lambda w: net.loss(x, t)
-dW = numerical_gradient(f, net.W)
-print(dW)
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y ,t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+# 4.5.2 ミニバッチ学習の実装
+# (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+#
+# train_loss_list = []
+#
+# iters_num = 10000
+# train_size = x_train.shape[0]
+# batch_size = 100
+# learning_rate = 0.1
+#
+# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+#
+# for i in range(iters_num):
+#     batch_mask = np.random.choice(train_size, batch_size)
+#     x_batch = x_train[batch_mask]
+#     t_batch = t_train[batch_mask]
+#
+#     grad = network.numerical_gradient(x_batch, t_batch)
+#
+#     for key in ('W1', 'b1', 'W2', 'b2'):
+#         network.params[key] -= learning_rate * grad[key]
+#
+#     loss = network.loss(x_batch, t_batch)
+#     train_loss_list.append(loss)
+
+# 4.5.3 テストデータで評価
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+train_size = x_train.shape[0]
+iters_num = 10000
+batch_size = 100
+learning_rate = 0.1
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
